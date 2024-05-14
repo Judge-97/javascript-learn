a3a8587601597f66fb666a48fea1ebec49185dfe
Judge-97 <50602352+Judge-97@users.noreply.github.com>
2024-05-14 15:26:14 +0000

    conversion

--- rtm/03_conversion.js
+++ rtm/03_conversion.js
@@ -1,11 +1,11 @@
 let score = "33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 // NaN is not a number
 // 33 convert to 33
 // 33abc convert to NaN
@@ -15,7 +15,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 0 and empty => false
 // 1 and fill => true
@@ -23,4 +23,35 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
+// console.log(stringNumber);
+
+// ********* Operationa **********
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+let str1 = "hello"
+let str2 = "rakesh"
+let str3 = str1 +str2
+
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+console.log(num1);
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
